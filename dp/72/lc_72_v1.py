72fc90ab242089e67a1078ee903abb7f6dd2b40e
yujingwen <lection.yu@gmail.com>
2019-07-28 02:40:09 +0800

    72 优化 打败 97%

--- dp/72/lc_72_v1.py
+++ dp/72/lc_72_v1.py
@@ -8,6 +8,8 @@ ab ca => 2
 ab cab => 1 当[i-1,j]的距离小于[i-1,j-1]的距离时，必然w1[i-1]==w2[j-1] 距离最多差1
 abb cab => 2
 这就为递归剪枝提供了可能性
+==========================
+用双数组优化取代m*n的数组矩阵初始化 加快速度 打败97%
 """
 
 
@@ -19,26 +21,22 @@ class Solution:
         if l1 == 0 or l2 == 0:
             return max(l1, l2)
 
-        dp = [[0] * l2 for _ in range(l1)]
+        prev_dp = [j for j in range(l2+1)]
+        cur_dp = [0] * (l2+1)
 
-        def fn(i, j):
-            if i < 0:
-                return j + 1
-            if j < 0:
-                return i + 1
-            return dp[i][j]
-
-        for i in range(l1):
-            for j in range(l2):
-                if word1[i] == word2[j]:
-                    dp[i][j] = fn(i-1, j-1)
+        for i in range(1, l1 + 1):
+            cur_dp[0] = i
+            for j in range(1, l2 + 1):
+                if word1[i-1] == word2[j-1]:
+                    cur_dp[j] = prev_dp[j-1]
                 else:
-                    dp[i][j] = 1 + min(
-                        fn(i-1, j),
-                        fn(i, j-1),
-                        fn(i-1, j-1)
+                    cur_dp[j] = 1 + min(
+                        prev_dp[j],
+                        cur_dp[j-1],
+                        prev_dp[j-1]
                     )
-        return dp[-1][-1]
+            cur_dp, prev_dp = prev_dp, cur_dp
+        return prev_dp[-1]
 
 
 s = Solution()
