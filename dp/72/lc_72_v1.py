7e74afae10b9b0bfab0567f4c34680c66aa5d927
yujingwen <lection.yu@gmail.com>
2019-07-27 22:22:37 +0800

    72 优化dp性能

--- dp/72/lc_72_v1.py
+++ dp/72/lc_72_v1.py
@@ -1,13 +1,20 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        dp = [[0] * (len(word2) + 1) for _ in range(len(word1) + 1)]
-        for i in range(len(word1) + 1):
+        l1 = len(word1)
+        l2 = len(word2)
+
+        if l1 == 0 or l2 == 0:
+            return max(l1, l2)
+
+        dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+
+        for i in range(l1 + 1):
             dp[i][0] = i
-        for j in range(len(word2) + 1):
+        for j in range(l2 + 1):
             dp[0][j] = j
 
-        for i in range(1, len(word1) + 1):
-            for j in range(1, len(word2) + 1):
+        for i in range(1, l1 + 1):
+            for j in range(1, l2 + 1):
                 dp[i][j] = 1 + min(
                     dp[i-1][j],
                     dp[i][j-1],
