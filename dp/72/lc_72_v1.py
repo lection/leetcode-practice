d034c2b5b74a6944b3475a2471e8963fe7f1eb9d
yujingwen <lection.yu@gmail.com>
2019-07-28 02:01:41 +0800

    72 优化一下速度 似乎有点儿效果

--- dp/72/lc_72_v1.py
+++ dp/72/lc_72_v1.py
@@ -6,19 +6,23 @@ class Solution:
         if l1 == 0 or l2 == 0:
             return max(l1, l2)
 
-        dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+        dp = [[0] * l2 for _ in range(l1)]
 
-        for i in range(l1 + 1):
-            dp[i][0] = i
-        for j in range(l2 + 1):
-            dp[0][j] = j
+        def fn(i, j):
+            if i < 0 and j < 0:
+                return 0
+            if i < 0:
+                return j + 1
+            if j < 0:
+                return i + 1
+            return dp[i][j]
 
-        for i in range(1, l1 + 1):
-            for j in range(1, l2 + 1):
+        for i in range(l1):
+            for j in range(l2):
                 dp[i][j] = 1 + min(
-                    dp[i-1][j],
-                    dp[i][j-1],
-                    dp[i-1][j-1] - (1 if word1[i-1] == word2[j-1] else 0)
+                    fn(i-1, j),
+                    fn(i, j-1),
+                    fn(i-1, j-1) - (1 if word1[i] == word2[j] else 0)
                 )
 
         return dp[-1][-1]
