98a8ef4ad864eacef07e7ce4e094e857ec547706
yujingwen <lection.yu@gmail.com>
2019-07-28 02:30:35 +0800

    72 优化dp

--- dp/72/lc_72_v1.py
+++ dp/72/lc_72_v1.py
@@ -1,3 +1,16 @@
+"""
+优化速度
+尝试减少初始化的时间 将空字符串的情况用判断解决
+==========================
+正常的递推公式 dp[i][j] = 1 + min( fn(i-1, j), fn(i, j-1), fn(i-1, j-1) + w1[i]==w2[j]?-1:0)
+但是 实际 当 w1[i] == w2[j] 的时候，相当于直接等于 dp[i-1][j-1]
+ab ca => 2
+ab cab => 1 当[i-1,j]的距离小于[i-1,j-1]的距离时，必然w1[i-1]==w2[j-1] 距离最多差1
+abb cab => 2
+这就为递归剪枝提供了可能性
+"""
+
+
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         l1 = len(word1)
@@ -9,8 +22,6 @@ class Solution:
         dp = [[0] * l2 for _ in range(l1)]
 
         def fn(i, j):
-            if i < 0 and j < 0:
-                return 0
             if i < 0:
                 return j + 1
             if j < 0:
@@ -19,12 +30,14 @@ class Solution:
 
         for i in range(l1):
             for j in range(l2):
-                dp[i][j] = 1 + min(
-                    fn(i-1, j),
-                    fn(i, j-1),
-                    fn(i-1, j-1) - (1 if word1[i] == word2[j] else 0)
-                )
-
+                if word1[i] == word2[j]:
+                    dp[i][j] = fn(i-1, j-1)
+                else:
+                    dp[i][j] = 1 + min(
+                        fn(i-1, j),
+                        fn(i, j-1),
+                        fn(i-1, j-1)
+                    )
         return dp[-1][-1]
 
 
