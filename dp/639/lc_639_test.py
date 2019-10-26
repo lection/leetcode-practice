4a6f0c2c6e40035c1141ae0488e075e75f14c082
Lection Yu <lection.yu@gmail.com>
2019-10-27 04:05:59 +0800

    test 639

--- dp/639/lc_639_test.py
+++ dp/639/lc_639_test.py
@@ -1,39 +1,3 @@
-"""
-一条包含字母 A-Z 的消息通过以下的方式进行了编码：
-
-'A' -> 1
-'B' -> 2
-...
-'Z' -> 26
-除了上述的条件以外，现在加密字符串可以包含字符 '*'了，字符'*'可以被当做1到9当中的任意一个数字。
-
-给定一条包含数字和字符'*'的加密信息，请确定解码方法的总数。
-
-同时，由于结果值可能会相当的大，所以你应当对10^9 + 7取模。（翻译者标注：此处取模主要是为了防止溢出）
-
-示例 1 :
-
-输入: "*"
-输出: 9
-解释: 加密的信息可以被解密为: "A", "B", "C", "D", "E", "F", "G", "H", "I".
-示例 2 :
-
-输入: "1*"
-输出: 9 + 9 = 18（翻译者标注：这里1*可以分解为1,* 或者当做1*来处理，所以结果是9+9=18）
-说明 :
-
-1. 输入的字符串长度范围是 [1, 105]。
-2. 输入的字符串只会包含字符 '*' 和 数字'0' - '9'。
-
-===========================
-还是先尝试从递归开始 和91差不多
-很复杂……比想象的麻烦 1* *1 ** *** 都很难处理
-不能再按91的递归方式去处理 要用更复杂的方式 遇到*则要看*是单独使用还是结合后面的元素组成两位数
-===========================
-总有新的用例让你痛不欲生
-*0 让*只有1和2两种选项
-*10 让*1的组合不可能出现
-"""
 import sys
 sys.setrecursionlimit(10000000)
 M_N = 10 ** 9 + 7
@@ -46,11 +10,9 @@ class Solution:
             return 0
 
         last_index = len(s) - 1
-        cache = [None] * len(s)
 
-        # prev_num表示idx是否与之前的字符组CP，如果组了，之前字符的是什么。
         def rec(idx, prev_num):
-            if idx > last_index:
+            if last_index < idx:
                 return 1
             c = s[idx]
             if prev_num is not None:
@@ -73,9 +35,6 @@ class Solution:
             if idx == last_index:
                 return 9 if c == '*' else 1
 
-            if cache[idx] is not None:
-                return cache[idx]
-
             r = rec(idx + 1, None)
             if c == '1' or c == '2':
                 r = r + rec(idx + 1, c)
@@ -84,9 +43,6 @@ class Solution:
                     r = 2 * rec(idx + 2, None)
                 else:
                     r = 9 * r + rec(idx + 1, '1') + rec(idx + 1, '2')
-
-            r = r % M_N
-            cache[idx] = r
             return r
 
         return rec(0, None)
