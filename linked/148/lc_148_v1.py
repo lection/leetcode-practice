4687b5750622ed32892b029cf726e3a434c4fb8e
Lection Yu <lection.yu@gmail.com>
2019-11-27 18:29:45 +0800

    148 归并排序

--- linked/148/lc_148_v1.py
+++ linked/148/lc_148_v1.py
@@ -12,9 +12,43 @@
 
 ========
 链表排序 用 归并排序 还不需要向数组那样申请额外的空间
-难点是链表不像数组可以轻易的分成两部分
+难点是链表不像数组可以轻易的分成两部分 我琢磨琢磨
+如果用快慢指针的方式 可以找到中间点 时间复杂度 n * log(n)
+========
+轻松通过 就是代码不够简洁
 """
+from utils import list_util
+from utils.list_util import ListNode
+
 
 class Solution:
     def sortList(self, head):
-        pass
+        if not head:
+            return None
+
+        def rec(node):
+            if node.next is None:
+                return node
+            mid = node
+            fast = node
+            while fast and fast.next:
+                last = mid
+                mid = mid.next
+                fast = fast.next.next
+            last.next = None
+            n1 = rec(node)
+            n2 = rec(mid)
+            result = node = ListNode(0)
+            while n1 and n2:
+                if n1.val > n2.val:
+                    n1, n2 = n2, n1
+                node.next = n1
+                node = node.next
+                n1 = n1.next
+            node.next = n2
+            return result.next
+        return rec(head)
+
+
+s = Solution()
+print(list_util.linked2list(s.sortList(list_util.list2linked([4, 2, 1, 3]))) == [1, 2, 3, 4])
